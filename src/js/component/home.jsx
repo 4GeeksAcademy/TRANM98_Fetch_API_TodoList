99bc60ae0641655114f3e415f5402256c0b7da72
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-05-10 00:16:56 +0000

    blank screen error

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr")
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const inputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addTask = () => {
+    if (inputValue.trim() === "") {
+      return;
+    }
+    setTasks([...tasks, { title: inputValue.trim(), completed: false }]);
+    setInputValue("");
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  };
+
+  const toggleComplete = (index) => {
+    setTasks(
+      tasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const incompleteTask = tasks.filter((task) => !task.completed).length;
+
+  return (
+    <div className="App">
+      <h1>todos</h1>
+      <div className="todo-box">
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="What needs to be done?"
+            value={inputValue}
+            onChange={inputChange}
+            onKeyPress={(event) => {
+              if (event.key === "Enter") {
+                addTask();
+              }
+            }}
+          />
+        </div>
+        <TodoList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleComplete={toggleComplete}
+        />
+        <div className="task-left">{incompleteTask} items left</div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
